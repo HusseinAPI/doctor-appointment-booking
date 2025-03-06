5e598c4c84b65bcdf2f03d86b3ef345de0087d04
hussein <httm010@gmail.com>
2025-03-06 23:29:19 +0200

    Design navbar and start home page layout

---
+++ src/app/Components/NavBar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaBriefcaseMedical } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <div className="sticky flex justify-around w-full h-16 bg-gradient-to-r from-blue-900 to-blue-600">
+      <div className="flex justify-center items-center text-yellow-100 text-xl">
+        <FaBriefcaseMedical className="mr-2" />
+        MedicalCenter
+      </div>
+      <div className="m-4 w-5/12 h-max text-teal-50">
+        <div className="flex justify-between items-center">
+          <span className="hover:text-teal-300">Home</span>
+          <span className="hover:text-teal-300">About Us</span>
+          <span className="hover:text-teal-300">Doctors</span>
+          <span className="hover:text-teal-300">Contact Us</span>
+          <span className="bg-yellow-50 hover:bg-yellow-200 text-blue-700 px-5 py-2 rounded-xl">
+            login
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,32 @@
-import Image from "next/image";
+import doctorImg from '../../public/doctor.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+    <div className="flex bg-gradient-to-r from-blue-900 to-blue-600 w-full h-[500px]">
+      <div className="flex justify-center w-1/2 mt-64 sm:mt-20 md:mt-40 lg:mt-48 xl:mt-14">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <defs></defs>
+          <path
+            fill="#FFFFFF"
+            d="M46.2,-79.8C60.2,-71.9,72.1,-60.2,79.1,-46.3C86.1,-32.3,88.2,-16.2,88.2,0C88.3,16.2,86.2,32.4,79.4,46.7C72.6,60.9,61,73.4,47,81.6C33,89.9,16.5,94,0.2,93.6C-16.1,93.3,-32.2,88.5,-45.8,80C-59.4,71.5,-70.4,59.3,-78,45.3C-85.6,31.4,-89.7,15.7,-89.9,-0.1C-90,-15.9,-86.2,-31.7,-78.7,-45.8C-71.2,-59.9,-60,-72.3,-46.3,-80.3C-32.6,-88.3,-16.3,-92,-0.1,-91.8C16.1,-91.7,32.2,-87.7,46.2,-79.8Z"
+            transform="translate(100 100)"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <image href={doctorImg.src} x="-15" y="10" width="250" height="250" />
+        </svg>
+      </div>
+      <div className="mt-24 w-1/2  text-teal-50">
+        <h1 className="text-6xl font-bold">
+          The Best Reliable <br />
+          Medical Service
+        </h1>
+        <span className="text-xl mt-2 block">
+          Stay on top of your health by connecting with
+          <br /> your doctor and easily schelduling a check-up
+        </span>
+        <button className="bg-yellow-50 hover:bg-yellow-200 text-blue-700 mt-4 px-5 py-2 rounded-md">
+          Make an Appointment
+        </button>
+      </div>
     </div>
   );
 }
