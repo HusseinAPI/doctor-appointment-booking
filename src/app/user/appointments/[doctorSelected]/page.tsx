c0878fb77fac62cc0fe40b796888720e2d60093b
hussein <hussein.a.kassab@gmail.com>
2025-04-16 02:57:25 +0300

    Appointment date and time (frontend)

--- src/app/user/appointments/[doctorSelected]/page.tsx
+++ src/app/user/appointments/[doctorSelected]/page.tsx
@@ -1,10 +1,13 @@
 'use client';
-import React, { useState } from 'react';
-import doctor102 from '../../../../../public/doctor102.jpg';
+import React, { useState, useEffect } from 'react';
+import doctor from '../../../../../public/doctor104.jpg';
 import Image from 'next/image';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
 
-const page = () => {
+const Page = () => {
+  // calendar setting
+
   const [currentDate, setCurrentDate] = useState(dayjs());
 
   const startOfMonth = currentDate.startOf('month');
@@ -28,42 +31,79 @@ const page = () => {
 
   const handleNextMonth = () => setCurrentDate(currentDate.add(1, 'month'));
 
+  // default days of the week
+
+  useEffect(() => {
+    const today = dayjs();
+
+    defaultAndUpdateDates(today);
+  }, []);
+
+  // select day from calendar
+
+  const handleSelectDay = (day: number) => {
+    const selectDay = dayjs(
+      new Date(currentDate.year(), currentDate.month(), day)
+    );
+
+    defaultAndUpdateDates(selectDay);
+  };
+
+  // default and update dates function
+
+  const [dates, setDates] = useState<string[]>([]);
+
+  const defaultAndUpdateDates = (date: Dayjs) => {
+    const Date = date;
+
+    const Dates = Array.from({ length: 10 }, (_, i) => Date.add(i, 'day'))
+      .filter((date) => date.day() !== 0 && date.day() !== 6)
+      .slice(0, 5)
+      .map((date) => date.format('ddd D MMM'));
+
+    setDates(Dates);
+  };
+
+  const times = ['10:30', '11:30', '12:30', '13:30', '14:30', '15:30', '16:00'];
+
   return (
-    <div className="h-[738px] rounded-l-2xl">
+    <div className="rounded-l-2xl">
       <div className="fixed overflow-auto left-20 rounded-4xl bg-blue-100 w-full h-full">
-        <div className="bg-gray-50 p-6 flex justify-around flex-col md:flex-row gap-8">
+        <div className="bg-white p-6 flex justify-around flex-col md:flex-row gap-8">
           {/* Doctor Card */}
-          <div className="bg-white rounded-2xl shadow-md p-6 w-1/2">
-            <div className="flex items-center gap-4">
+          <div className="flex bg-white rounded-2xl shadow-md p-6 w-1/2">
+            <div className="flex items-center w-9/12">
               <Image
-                src={doctor102.src}
+                src={doctor.src}
                 alt="doctor"
-                className="rounded-full w-20 h-20 object-cover"
-                width={0}
-                height={0}
+                className="rounded-xl w-96 h-80"
+                width={500}
+                height={500}
               />
-              <div>
-                <h2 className="text-lg font-semibold">Dr. Alexander Kimon</h2>
-                <p className="text-sm text-gray-500">General Dentist</p>
-              </div>
             </div>
-            <p className="mt-4 text-sm italic text-gray-600">
-              Best dental appointment I have had in years, seriously. I would
-              highly...
-            </p>
+            <div className="flex flex-wrap p-2">
+              <div className="w-full">
+                <h2 className="text-lg font-semibold">Dr. Alexander Kimon</h2>
+                <span className="text-sm text-gray-500">General Dentist</span>
+              </div>
+              <span className="text-sm italic text-gray-600">
+                Best dental appointment I have had in years, seriously. I would
+                highly...
+              </span>
+            </div>
           </div>
 
           {/* Calendar */}
-          <div className="bg-amber-300 rounded-2xl shadow-md p-6 w-full max-w-md">
+          <div className="bg-blue-600 rounded-2xl shadow-md p-6 w-full max-w-md">
             <div className="flex justify-between items-center mb-4">
               <button onClick={handlePrevMonth} className="text-xl text-white">
-                &lt;
+                <MdArrowBackIos />
               </button>
               <h3 className="text-lg font-semibold text-white">
                 {currentDate.format('MMMM') + ' ' + currentDate.year()}
               </h3>
               <button onClick={handleNextMonth} className="text-xl text-white">
-                &gt;
+                <MdArrowForwardIos />
               </button>
             </div>
 
@@ -88,10 +128,13 @@ const page = () => {
                     className={`rounded-lg p-2 h-10 flex items-center justify-center ${
                       val
                         ? isWeekend
-                          ? 'bg-blue-500 text-white'
+                          ? 'bg-gray-400 text-gray-500 cursor-not-allowed'
                           : 'bg-white text-gray-800 hover:bg-amber-600 hover:text-white'
-                        : 'bg-amber-200 text-transparent'
+                        : 'bg-gray-400 text-transparent'
                     }`}
+                    onClick={() =>
+                      val ? (!isWeekend ? handleSelectDay(val) : null) : null
+                    }
                   >
                     {val}
                   </div>
@@ -100,10 +143,43 @@ const page = () => {
             </div>
           </div>
         </div>
-        <div className="w-full h-72 bg-amber-300"></div>
+
+        {/*Date and Time*/}
+        <div className="bg-white w-full flex flex-wrap p-10">
+          <h2 className="text-xl font-medium w-full mb-4 ml-12">
+            Choose Date and Time
+          </h2>
+          <div className="flex justify-center flex-wrap w-9/12 ml-12 rounded-xl bg-gray-200 border border-gray-200 shadow">
+            <div className="w-full flex justify-between text-white font-medium bg-blue-600 rounded-t-xl p-2 px-4">
+              <div className="w-12 h-9" />
+              {dates.map((day, index) => (
+                <span key={index} className="text-center w-15">
+                  {day}
+                </span>
+              ))}
+              <div className="w-12 h-9" />
+            </div>
+            <div className="w-3/4 flex justify-between text-white font-medium rounded-b-xl">
+              {dates.map((day, index) => (
+                <div key={index}>
+                  {times.map((time, index) => (
+                    <div className="m-2" key={index}>
+                      <div className="flex flex-wrap flex-col w-1/6">
+                        <button className="w-35 py-1 text-base rounded-lg bg-white border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white  cursor-pointer">
+                          {time}
+                        </button>
+                      </div>
+                      <div className="absolute top-0 right-0 h-full border-r border-gray-200" />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
